2544a2bcb48ae4a271501d3ede4a38d9933532fd
min-38 <mindino03@gmail.com>
2023-08-01 02:30:45 +0900

    #3 로그인 페이지 UI 구현
    간단한 css 적용

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('{path}', 'index')->where('path', '([A-z\d\-\/_.]+)?');
+
+// Route::get('/', function () {
+//     return view('index');
+// });
